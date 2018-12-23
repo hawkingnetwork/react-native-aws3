2605e148b1c3505610c0e26cbe74eb8c6ca621e0
naviocean <navi.ocean@outlook.com>
2018-12-23 09:34:51 +0700

    update dependencies & fix url

--- src/RNS3.js
+++ src/RNS3.js
@@ -2,49 +2,53 @@
  * RNS3
  */
 
-import { Request } from './Request'
-import { S3Policy } from './S3Policy'
+import { Request } from './Request';
+import { S3Policy } from './S3Policy';
 
-const AWS_DEFAULT_S3_HOST = 's3.amazonaws.com'
+const AWS_DEFAULT_S3_HOST = 's3.amazonaws.com';
 
 const EXPECTED_RESPONSE_KEY_VALUE_RE = {
   key: /<Key>(.*)<\/Key>/,
   etag: /<ETag>"?([^"]*)"?<\/ETag>/,
   bucket: /<Bucket>(.*)<\/Bucket>/,
-  location: /<Location>(.*)<\/Location>/,
-}
+  location: /<Location>(.*)<\/Location>/
+};
 
-const entries = o =>
-  Object.keys(o).map(k => [k, o[k]])
+const entries = o => Object.keys(o).map(k => [k, o[k]]);
 
-const extractResponseValues = (responseText) =>
+const extractResponseValues = responseText =>
   entries(EXPECTED_RESPONSE_KEY_VALUE_RE).reduce((result, [key, regex]) => {
-    const match = responseText.match(regex)
-    return { ...result, [key]: match && match[1] }
-  }, {})
+    const match = responseText.match(regex);
+    return { ...result, [key]: match && match[1] };
+  }, {});
 
-const setBodyAsParsedXML = (response) =>
-  ({
-    ...response,
-    body: { postResponse: response.text == null ? null : extractResponseValues(response.text) }
-  })
+const setBodyAsParsedXML = response => ({
+  ...response,
+  body: {
+    postResponse:
+      response.text == null ? null : extractResponseValues(response.text)
+  }
+});
 
 export class RNS3 {
   static put(file, options) {
     options = {
       ...options,
       key: (options.keyPrefix || '') + file.name,
-      date: new Date,
+      date: new Date(),
       contentType: file.type
-    }
+    };
 
-    const url = `https://${options.bucket}.${options.awsUrl || AWS_DEFAULT_S3_HOST}`
-    const method = "POST"
-    const policy = S3Policy.generate(options)
-
+    const url =
+      options.region !== '' && options.region !== 'us-east-1'
+        ? `https://s3-${options.region}.amazonaws.com/${options.bucket}`
+        : `https://${options.bucket}.${options.awsUrl || AWS_DEFAULT_S3_HOST}`;
+    const method = 'POST';
+    const policy = S3Policy.generate(options);
+    console.log(url);
     return Request.create(url, method, policy)
-      .set("file", file)
+      .set('file', file)
       .send()
-      .then(setBodyAsParsedXML)
+      .then(setBodyAsParsedXML);
   }
 }
